85dc406128afe728fa0bff74074d8b2f1d71700d
julia sheremet <juliajessicasher@gmail.com>
2018-08-30 11:08:02 -0700

    push to github

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="/">
-        NAVBAR{" "}
+        Items In Cart{" "}
         <span className="badge badge-pill badge-secondary">
           {totalCounters}
         </span>
